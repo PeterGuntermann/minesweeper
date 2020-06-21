e364cdcfbc5db2cc8f4d831ad5702d50786ff5c0
Peter Guntermann <pguntermann@andrena.de>
2020-06-21 17:03:27 +0200

    Create `Field` component that reveals on click
    Styling still missing

--- src/components/Minesweeper.js
+++ src/components/Minesweeper.js
@@ -1,21 +1,67 @@
 import * as React from "react";
 
 export class Minesweeper extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
 
-    renderField = () => (<strong>field</strong>);
+    render() {
+        return (<>
+            <FieldContainer value={3}/>
+            <FieldContainer value={2}/>
+            <FieldContainer value={6}/>
+            <FieldContainer value={1}/>
+            <FieldContainer value={0}/>
+            <FieldContainer value={0}/>
+            <FieldContainer value={0}/>
+            <FieldContainer value={0}/>
+            <FieldContainer value={1}/>
+            <FieldContainer value={5}/>
+            <FieldContainer value={2}/>
+            <FieldContainer value={3}/>
+            <FieldContainer value={6}/>
+            <FieldContainer value={8}/>
+            <FieldContainer value={3}/>
+            <FieldContainer value={6}/>
+            <FieldContainer value={3}/>
+            <FieldContainer value={1}/>
+            <FieldContainer value={2}/>
+        </>);
+    }
+}
+
+class FieldContainer extends React.Component {
+    state = {
+        isRevealed: false
+    };
+
+    handleReveal = () => {
+        this.setState({isRevealed: true})
+    }
 
 
     render() {
-        return (
-            <>
-                <p>Yo!</p>
-                <div>
-                    {this.renderField()}
-                </div>
-            </>
-        );
+        const {isRevealed} = this.state;
+        const {value} = this.props;
+        return <Field value={value} isRevealed={isRevealed} onReveal={this.handleReveal}/>;
     }
 }
+
+const Field = ({isRevealed, value, onReveal}) => (
+    isRevealed
+        ? <RevealedField value={value}/>
+        : <UnrevealedField onReveal={onReveal}/>
+);
+
+const RevealedField = ({value}) => (
+    <div className={`revealed-field neighbor-count-${value}`}>
+        <span>{value}</span>
+    </div>
+);
+
+const UnrevealedField = ({onReveal}) => (
+    <div className="unrevealed-field" onClick={onReveal}>
+        ?
+    </div>
+);
