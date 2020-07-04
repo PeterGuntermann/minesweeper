32e6a8faee812f6c5787b9baf7ca9f98b2edd4ee
Peter Guntermann <pguntermann@andrena.de>
2020-07-04 20:27:35 +0200

    Set up basic testing

---
+++ src/components/__test__/Minesweeper.test.ts
@@ -0,0 +1,17 @@
+import { unmountComponentAtNode } from "react-dom";
+
+let container: any = null;
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+test('should', () => {
+    expect(true).toBe(true);
+});
