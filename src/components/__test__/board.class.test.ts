26e950a80b4b498fef985a0c15ddd0d228bf50ad
Peter Guntermann <pguntermann@andrena.de>
2020-07-10 17:22:07 +0200

    Roll minePositions and test it

--- src/components/__test__/board.class.test.ts
+++ src/components/__test__/board.class.test.ts
@@ -3,10 +3,11 @@ import { Board } from '../board.class';
 describe('Board', () => {
     test('should initialize the positions of the fields correctly', () => {
         const board = new Board(2, 2);
-        const fields = board.allFields;
 
         expect(board.numberOfFields).toBe(4);
-        expect(fields.length).toEqual(board.numberOfFields);
+
+        const fields = board.allFields;
+        expect(fields).toHaveLength(board.numberOfFields);
 
         expect(fields[0].position.x).toEqual(0);
         expect(fields[0].position.y).toEqual(0);
@@ -21,7 +22,36 @@ describe('Board', () => {
         expect(fields[3].position.y).toEqual(1);
     });
 
-    test('should ', () => {
-        //
+    test('should roll mine positions that are all different', () => {
+        const numberOfColumns = 3;
+        const numberOfRows = 2;
+        const numberOfMines = 4;
+
+        const board = new Board(numberOfColumns, numberOfRows, numberOfMines);
+
+        const minePositions = board.allMinePositions;
+        expect(minePositions).toHaveLength(numberOfMines);
+        expect(minePositions.every((pos) => pos.x < numberOfColumns)).toBe(
+            true
+        );
+        expect(minePositions.every((pos) => pos.y < numberOfRows)).toBe(true);
+
+        for (let i = 0; i < board.numberOfMines; i++) {
+            for (let j = 0; j < board.numberOfMines; j++) {
+                if (i === j) continue;
+                const minePositionsAreIdentical =
+                    minePositions[i].x === minePositions[j].x &&
+                    minePositions[i].y === minePositions[j].y;
+                expect(minePositionsAreIdentical).toBe(false);
+            }
+        }
+    });
+
+    test('should distribute the mines over the corresponding fields', () => {
+        const numberOfColumns = 3;
+        const numberOfRows = 2;
+        const numberOfMines = 4;
+
+        const board = new Board(numberOfColumns, numberOfRows, numberOfMines);
     });
 });
--- src/components/board.class.ts
+++ src/components/board.class.ts
@@ -1,14 +1,20 @@
 import { FieldModel } from '../types/field.interface';
+import { Position } from '../types/position.interface';
 
 export class Board {
     private fields: FieldModel[];
+    private minePositions: Position[] = new Array<Position>();
 
     constructor(
+        public readonly numberOfColumns: number,
         public readonly numberOfRows: number,
-        public readonly numberOfColumns: number
+        public readonly numberOfMines: number = 0
     ) {
         this.fields = [];
-        this.initializeFields();
+        this.minePositions = [];
+        this.initializeBlankFields();
+        this.rollMinePositions();
+        this.distributeMines();
     }
 
     get numberOfFields(): number {
@@ -19,7 +25,11 @@ export class Board {
         return this.fields;
     }
 
-    private initializeFields() {
+    get allMinePositions(): Position[] {
+        return this.minePositions;
+    }
+
+    private initializeBlankFields() {
         for (let i = 0; i < this.numberOfFields; i++) {
             this.fields.push(<FieldModel>{
                 hasMine: false,
@@ -30,5 +40,29 @@ export class Board {
                 },
             });
         }
+    }
+
+    private rollMinePositions() {
+        while (this.minePositions.length < this.numberOfMines) {
+            const randomColumn = Math.floor(Math.random() * this.numberOfRows);
+            const randomRow = Math.floor(Math.random() * this.numberOfRows);
+            const position: Position = {
+                x: randomColumn,
+                y: randomRow,
+            };
+            const positionAlreadyExists = this.minePositions.some(
+                (pos) => pos.x === randomColumn && pos.y === randomRow
+            );
+
+            if (!positionAlreadyExists) {
+                this.minePositions.push(position);
+            }
+        }
+    }
+
+    private distributeMines() {
+        this.minePositions.forEach((minePosition) => {
+            console.log(minePosition);
+        });
     }
 }
