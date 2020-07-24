48a57fdff2a5a2ae5e13966cf3a3b6c77b36eadd
Peter Guntermann <pguntermann@andrena.de>
2020-07-24 21:15:43 +0200

    Add Bootstrap

--- src/components/Minesweeper.tsx
+++ src/components/Minesweeper.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import '../styles/minesweeper.scss';
-import { FieldModel } from '../types/field.interface';
 import { Level } from '../types/level.enum';
-import { Board } from './board.class';
 import { Field } from './Field';
+import { FieldModel } from '../types/field.interface';
+import { Board } from './board.class';
+import { Button, ButtonGroup, ToggleButton } from 'react-bootstrap';
 
 interface MinesweeperProps {}
 
@@ -55,17 +56,38 @@ export class Minesweeper extends React.Component<MinesweeperProps, MinesweeperSt
     };
 
     // TODO: Invoke action via button instead of selection change
-    levelChooser = () => (
-        <div className="choose-level">
-            <label>Level: </label>
-            <select value={this.state.level} onChange={this.handleChangeLevel}>
-                <option value="">-- Choose difficulty --</option>
-                <option value="easy">Easy: 9x9 board, 10 mines (12%)</option>
-                <option value="medium">Medium: 16x16 board, 40 mines (16%)</option>
-                <option value="hard">Hard: 30x16 board, 100 mines (21%)</option>
-            </select>
-        </div>
-    );
+    levelChooser = () => {
+        const radioOptions = [
+            { name: 'Easy', value: '1' },
+            { name: 'Normal', value: '2' },
+            { name: 'Hard', value: '3' },
+        ];
+        return (
+            <div className="choose-level">
+                <Button variant="primary">Primary</Button> <label>Level: </label>
+                <ButtonGroup toggle>
+                    {radioOptions.map((radio, idx) => (
+                        <ToggleButton
+                            key={idx}
+                            type="radio"
+                            variant="secondary"
+                            name="radio"
+                            value={radio.value}
+                            checked={idx === 1}
+                        >
+                            {radio.name}
+                        </ToggleButton>
+                    ))}
+                </ButtonGroup>
+                <select value={this.state.level} onChange={this.handleChangeLevel}>
+                    <option value="">-- Choose difficulty --</option>
+                    <option value="easy">Easy: 9x9 board, 10 mines (12%)</option>
+                    <option value="medium">Medium: 16x16 board, 40 mines (16%)</option>
+                    <option value="hard">Hard: 30x16 board, 100 mines (21%)</option>
+                </select>
+            </div>
+        );
+    };
 
     render() {
         return (
