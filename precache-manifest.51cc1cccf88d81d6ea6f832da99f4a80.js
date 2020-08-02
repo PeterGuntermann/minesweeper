7adb5ee3b2dd66ce3be37622777f3b78a0750cf3
Peter Guntermann <pgu@posteo.de>
2020-08-02 21:31:53 +0200

    Updates

---
+++ precache-manifest.51cc1cccf88d81d6ea6f832da99f4a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc514d87263febeb54ac6b1e9e696e3",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "7849a50358930122ce4d",
+    "url": "/minesweeper/static/css/main.aeab7457.chunk.css"
+  },
+  {
+    "revision": "586f0b1d273543b7f406",
+    "url": "/minesweeper/static/js/2.9c01a499.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/minesweeper/static/js/2.9c01a499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7849a50358930122ce4d",
+    "url": "/minesweeper/static/js/main.3233d574.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
